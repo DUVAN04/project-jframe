037b926fcd8616b7a14e0de41a78a2c9ffb84b32
kenteshi <joeduvan30@gmail.com>
2025-09-24 11:38:57 -0500

    project finish interface visual jframe

---
+++ src/main/java/cun/edu/co/ProjectFrame.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cun.edu.co;
+
+/**
+ *
+ * @author thor
+ */
+import cun.edu.co.designer.Screen;
+public class ProjectFrame {
+
+    public static void main(String[] args) {
+        Screen screen = new Screen();
+        screen.setVisible(true);
+        screen.setLocationRelativeTo(null);
+    }
+}
